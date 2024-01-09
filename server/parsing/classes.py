099af40fef29d2b5fc1fbc3c8b850d8eb327f385
BryanOcto <>
2024-01-09 22:48:15 +1100

    Revised subject data representation
    - There was a conceptual mistake between streams and stream groups; that now has been corrected. So the structure is moved around a bit to now more accurately represent the data.
    - Stream groups are now part of the data structure, since it currently seems possible to get data for that, thus the user does not need to manually specify it.

--- server/parsing/classes.py
+++ server/parsing/classes.py
@@ -5,39 +5,54 @@ from datetime import time, date
 
 @dataclass
 class Times:
-    """Stores an Activity's duration"""
+    """Stores an Activity's start and end times"""
 
     start: time
     end: time
 
 
 @dataclass
-class Activity:
+class Stream:
     """Stores one instance of a lecture/tute"""
 
     code: int
-    name: str  # e.g. "Lecture 1" or "Practical 3"
-    atype: str  # e.g. "Lecture recording" or "Tutorial"
+
+    weeks: List[int]  # e.g. 1, etc. converted using `weeks-config.json`
 
     day: str  # e.g. "Mon", etc. using .strftime("%A"); thanks StackOverflow
     times: Times
+
     location: str
     online: bool
 
 
 @dataclass
-class Stream:
-    """Stores the Activity's that make up a lecture/tute stream"""
+class Activity:
+    """Stores the different Stream's of an Activity"""
 
-    code: int
-    weeks: List[int]  # e.g. 1, etc. converted using `weeks-config.json`
+    atype: str  # e.g. "Lecture recording" or "Tutorial"
+    name: str  # e.g. "Lecture 1" or "Practical 3"
+    stream_list: List[Stream]
+
+
+@dataclass
+class ActivityGroup:
+    """Stores the activities whose streams must be picked at the same time"""
+
+    name: str  # e.g. "Lecture" or "Tutorial" or "Practical" or "ComputerLab"
     activity_list: List[Activity]
 
 
 @dataclass
 class Subject:
-    """Stores the lecture/tute streams of a subject"""
+    """
+    Stores a subject's details and ActivityGroups.
+    Note that some subjects don't e.g. group their lectures,
+    e.g. 2024 Summer Statistics.
+    """
 
-    code: str
-    offering: str  # e.g. "Semester 1", "Summer Term", etc. as listed in CSV
-    stream_list: List[Stream]
+    code: str  # e.g. "MAST20005"
+    name: str  # e.g. "Statistics"
+    year: int  # e.g. 2024
+    offering: str  # e.g. "Semester 1", "Summer Term", etc. as seen in raw data
+    activity_group_list: List[ActivityGroup]
