20ee25964826e55f06c29e2a3a46a491adb95537
BryanOcto <>
2024-03-08 13:32:36 +1100

    Refactored parsing implementation (without time/week handling)
    Refactors:
    - More readable and concise (e.g. list comprehensions)
    - Better respects encapsulation
    - Tasks spread out over more methods instead of one big method
    
    Added a summer subject for testing activity groups and more activities
    
    Given sample output is generated from `parsingtest.py`

--- server/parsing/classes.py
+++ server/parsing/classes.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import List
+from typing import List, Dict, Set
 from datetime import time, date
 
 
@@ -14,72 +14,192 @@ class Times:
         """
         Returns dictionary version of itself for JSON serialization
         """
-        return {"start": time.isoformat(self.start), "end": time.isoformat(self.end)}
-
-
-@dataclass
-class Stream:
-    """Stores one instance of a lecture/tute"""
-
-    stream_id: int  # Unique within each activity; remember that a stream of an activity with the same id is paired with another stream of another activity with the same id if it is within an activity group.
-
-    weeks: List[int]  # e.g. 1, etc. converted using `weeks-config.json`
-
-    day: str  # e.g. "Mon", etc.
-    times: Times
-    location: str
-
-    def to_dict(self):
-        """
-        Returns dictionary version of itself for JSON serialization
-        """
         return {
-            "stream_id": self.stream_id,
-            "weeks": self.weeks,
-            "day": self.day,
-            "times": self.times.to_dict(),
-            "location": self.location,
+            "start": time.isoformat(self.start),
+            "end": time.isoformat(self.end),
         }
 
 
-@dataclass
-class Activity:
-    """Stores the different Stream's of an Activity"""
+class Stream:
+    """Stores one instance of a lecture/tute"""
 
-    activity_type: str  # e.g. "Lecture recording" or "Tutorial"
-    name: str  # e.g. "Lecture 1" or "Practical 3"
-    activity_id: int  # Unique for one entire Subject object
-    stream_list: List[Stream]
+    def __init__(self, raw: dict, raw_keys: List[str], stream_key: str):
+        """
+        Takes in raw data, keys, and the key to the stream and populates it
+        with appropriate details
+        """
+        # Note: stream_id is unique within each activity; remember that a stream of an activity with the same id is paired with another stream of another activity with the same id if it is within an activity group.
+        # e.g.s
+        # - weeks: [1, 2, ...] converted from parsing data
+        # - day: "Mon", etc.
+        self.__stream_id: int = int(
+            raw[stream_key]["activity_code"].split("-")[0]
+        )
+        self.__weeks: List[int] = self.__find_weeks()
+        self.__day: str = raw[stream_key]["day_of_week"]
+        self.__times: Times = self.__find_times()
+        self.__location: str = raw[stream_key]["location"]
+
+    def __find_weeks(self):
+        # TO-DO
+        return []
+
+    def __find_times(self):
+        # TO-DO
+        return Times(time(), time())
 
     def to_dict(self):
         """
         Returns dictionary version of itself for JSON serialization
         """
-        stream_list = [stream.to_dict() for stream in self.stream_list]
         return {
-            "activity_type": self.activity_type,
-            "name": self.name,
-            "activity_id": self.activity_id,
+            "stream_id": self.__stream_id,
+            "weeks": self.__weeks,
+            "day": self.__day,
+            "times": self.__times.to_dict(),
+            "location": self.__location,
+        }
+
+    # A getter method
+    @property
+    def stream_id(self):
+        return self.__stream_id
+
+
+class Activity:
+    """Stores the different Stream's of an Activity"""
+
+    __default_id = 1
+    __id = __default_id  # counter across one ActivityGroup object
+
+    def __init__(
+        self, raw: dict, raw_keys: List[str], activity_name_type: (str, str)
+    ):
+        """
+        Takes in raw data, keys, and the activity name and type and adds
+        appropriate streams
+        """
+        # e.g.s
+        # - activity_type: Lecture recording" or "Tutorial"
+        # - name: "Lecture 1" or "Practical 3"
+        # - activity_id: Unique for one entire Subject object
+        self.__activity_name: str = activity_name_type[0]
+        self.__activity_type: str = activity_name_type[1]
+        self.__activity_id: int = self.__use_id()
+        self.__stream_list: List[Stream] = self.__find_stream_list(
+            raw, raw_keys
+        )
+
+    def __find_stream_list(self, raw: dict, raw_keys: List[str]):
+        """
+        Takes in the keys to the dictionary and the dictionary from the JSON
+        which represents the various activities, and generates the
+        stream_list defined above.
+        """
+        # Get all streams in the activity,
+        # and generate all Stream objects;
+        # populating it is handled by Stream
+        # Assumes each stream is unique, so no sets here
+        stream_keys: List[(str, str)] = [
+            stream_key
+            for stream_key in raw_keys
+            if raw[stream_key]["description"] == self.__activity_name
+        ]
+        stream_list: List[Stream] = [
+            Stream(raw, raw_keys, stream_key) for stream_key in stream_keys
+        ]
+        # No resetting id's here since id is taken from raw data
+        # instead of manually generated
+        stream_list.sort(key=lambda Stream: Stream.stream_id)
+
+        return stream_list
+
+    def __use_id(self):
+        curr_id = Activity.__id
+        Activity.__id += 1
+        return curr_id
+
+    @classmethod
+    def reset_id(self):
+        Activity.__id = Activity.__default_id
+
+    def to_dict(self):
+        """
+        Returns dictionary version of itself for JSON serialization
+        """
+        stream_list = [stream.to_dict() for stream in self.__stream_list]
+        return {
+            "activity_type": self.__activity_type,
+            "name": self.__activity_name,
+            "activity_id": self.__activity_id,
             "stream_list": stream_list,
         }
 
 
-@dataclass
 class ActivityGroup:
     """Stores the activities whose streams must be picked at the same time"""
 
-    name: str  # e.g. "Lecture" or "Tutorial" or "Practical" or "ComputerLab"
-    group_id: int  # Unique for one entire Subject object
-    activity_list: List[Activity]
+    __default_id = 1
+    __id = __default_id  # counter across one Subject object
+
+    def __init__(
+        self, raw: dict, raw_keys: List[str], activity_group_name: str
+    ):
+        """
+        Takes in raw data, keys, and the group name and adds appropriate
+        activities
+        """
+        # e.g.s
+        # - name: "Lecture" or "Tutorial" or "Practical" or "ComputerLab"
+        # - group_id: Unique within one Subject object
+        self.__group_name: str = activity_group_name
+        self.__group_id: int = self.__use_id()
+        self.__activity_list: List[Activity] = self.__find_activity_list(
+            raw, raw_keys
+        )
+
+    def __use_id(self):
+        curr_id = ActivityGroup.__id
+        ActivityGroup.__id += 1
+        return curr_id
+
+    @classmethod
+    def reset_id(self):
+        ActivityGroup.__id = ActivityGroup.__default_id
+
+    def __find_activity_list(self, raw: dict, raw_keys: List[str]):
+        """
+        Takes in the keys to the dictionary and the dictionary from the JSON
+        which represents the various activities, and generates the
+        activity_list defined above.
+        """
+        # Get all activities in the activity group,
+        # and generate all Activity objects;
+        # populating it is handled by Activity
+        activity_names_types: Set[(str, str)] = {
+            (raw[key]["description"], raw[key]["activityType"])
+            for key in raw_keys
+            if raw[key]["activity_group_code"] == self.__group_name
+        }
+        activity_names_types: List[(str, str)] = sorted(activity_names_types)
+        activity_list: List[Activity] = [
+            Activity(raw, raw_keys, activity_name_type)
+            for activity_name_type in activity_names_types
+        ]
+        Activity.reset_id()
+
+        return activity_list
 
     def to_dict(self):
         """
         Returns dictionary version of itself for JSON serialization
         """
-        activity_list = [activity.to_dict() for activity in self.activity_list]
+        activity_list = [
+            activity.to_dict() for activity in self.__activity_list
+        ]
         return {
-            "name": self.name,
-            "group_id": self.group_id,
+            "name": self.__group_name,
+            "group_id": self.__group_id,
             "activity_list": activity_list,
         }
 
@@ -100,21 +220,15 @@ class Subject:
         raw_keys = sorted(raw.keys())
         first = raw_keys[0]
 
-        # code, e.g. "MAST10008"
+        # e.g.s
+        # - code: "MAST10008"
+        # - name: "Accelerated Mathematics 1"
+        # - year: 2024 (uses start date to determine)
+        # - offering: "Semester 1", "Summer Term", etc.
         self.__code: str = raw[first]["subject_code"].split("_")[0]
-
-        # name, e.g. "Accelerated Mathematics 1"
         self.__name: str = raw[first]["subject_description"]
-
-        # year, e.g. 2024
-        # uses the last 4 characters of the start date,
-        # assuming that's always the correct year.
-        self.__year: int = raw[first]["start_date"][-4:]
-
-        # offering: str, e.g. "Semester 1", "Summer Term", etc.
+        self.__year: int = int(raw[first]["start_date"][-4:])
         self.__offering: str = self.__find_offering(raw, first)
-
-        # activity_group_list: List[ActivityGroup]
         self.__activity_group_list: List[ActivityGroup] = (
             self.__find_activity_group_list(raw, raw_keys)
         )
@@ -138,77 +252,36 @@ class Subject:
                 # not found one that is not in the list
                 raise Exception("Invalid/unimplemented offering")
 
-    def __find_weeks(self):
-        # TO-DO
-        return []
-
-    def __find_times(self):
-        # TO-DO
-        return Times(time(), time())
-
-    def __find_activity_group_list(self, raw, raw_keys):
+    def __find_activity_group_list(self, raw: dict, raw_keys: List[str]):
         """
         Takes in the keys to the dictionary and the dictionary from the JSON
         which represents the various activities, and generates the
         activity_group_list defined above.
         """
-        # Citations
-        # - thanks for the list comprehension https://stackoverflow.com/questions/2739800/extract-list-of-attributes-from-list-of-objects-in-python
-        # - thanks for finding an index https://stackoverflow.com/questions/176918/how-to-find-the-index-for-a-given-item-in-a-list
-        activity_group_list: List[ActivityGroup] = []
-        activity_group_id: int = 1
-        activity_id: int = 1
-
-        for key in raw_keys:
-            # 1. Handle the ActivityGroup
-            activity_group_name: str = raw[key]["activity_group_code"]
-            activity_group_names = [
-                activity_group.name for activity_group in activity_group_list
-            ]
-
-            if activity_group_name not in activity_group_names:
-                # Create a new ActivityGroup object
-                activity_group_list.append(
-                    ActivityGroup(activity_group_name, activity_group_id, [])
-                )
-                activity_group_names.append(activity_group_name)
-                activity_group_id += 1
-            # This index is the same as in the object so we exploit that
-            activity_group_index = activity_group_names.index(activity_group_name)
-
-            # 2. Now that the ActivityGroup exists, consider the Activity
-            activity_type: str = raw[key]["activityType"]
-            activity_name: str = raw[key]["description"]
-            activity_names = [
-                activity.name
-                for activity in activity_group_list[activity_group_index].activity_list
-            ]
-
-            if activity_name not in activity_names:
-                # Create a new Activity
-                activity_group_list[activity_group_index].activity_list.append(
-                    Activity(activity_type, activity_name, activity_id, [])
-                )
-                activity_names.append(activity_name)
-                activity_id += 1
-            activity_index = activity_names.index(activity_name)
-
-            # 3. Finally, deal with the Stream once the two above exists
-            stream_id: int = raw[key]["activity_code"].split("-")[0]
-            weeks: List[int] = self.__find_weeks()
-            day: str = raw[key]["day_of_week"]
-            times: Times = self.__find_times()
-            location: str = raw[key]["location"]
-
-            activity_group_list[activity_group_index].activity_list[
-                activity_index
-            ].stream_list.append(Stream(stream_id, weeks, day, times, location))
+        # Get all activity groups in the subject,
+        # and generate all ActivityGroup objects;
+        # populating it is handled by ActivityGroup
+        activity_group_names: Set[str] = {
+            raw[key]["activity_group_code"] for key in raw_keys
+        }
+        activity_group_names: List[str] = sorted(activity_group_names)
+        activity_group_list: List[ActivityGroup] = [
+            ActivityGroup(raw, raw_keys, activity_group_name)
+            for activity_group_name in activity_group_names
+        ]
+        ActivityGroup.reset_id()
 
         return activity_group_list
 
     def __str__(self):
         return (
-            self.__code + " " + self.__name + ", " + self.__year + " " + self.__offering
+            self.__code
+            + " "
+            + self.__name
+            + ", "
+            + str(self.__year)
+            + " "
+            + self.__offering
         )
 
     def to_dict(self):
@@ -217,7 +290,8 @@ class Subject:
         """
         # Convert each element in activity group for serialization
         activity_group_list = [
-            activity_group.to_dict() for activity_group in self.__activity_group_list
+            activity_group.to_dict()
+            for activity_group in self.__activity_group_list
         ]
         return {
             "code": self.__code,
--- server/parsing/parsingtest.py
+++ server/parsing/parsingtest.py
@@ -1,26 +1,15 @@
 from classes import *
 import json
 
-with open("MAST10008-allocate.json", "r") as f:
+with open("MAST10007-allocate.json", "r") as f:
     data1 = json.load(f)
 
 with open("FNCE10002-allocate.json", "r") as f:
     data2 = json.load(f)
 
-MAST10008 = Subject(data1)
+MAST10007 = Subject(data1)
 FNCE10002 = Subject(data2)
-# print(MAST10008)
-print(FNCE10002)
 
-print(MAST10008._Subject__activity_group_list)
-
-
-# thanks https://stackoverflow.com/questions/26033239/list-of-objects-to-json-with-python
-# def obj_dict(obj):
-#     return vars(obj)
-
-
-# print(json.dumps(MAST10008.to_dict(), indent=2))
 print(json.dumps(FNCE10002.to_dict(), indent=2))
-# with open("dumptest.json", "w") as f:
-#     json.dump(MAST10008, f)
+with open("MAST10007-output.json", "w") as f:
+    json.dump(MAST10007.to_dict(), f, indent=2)
