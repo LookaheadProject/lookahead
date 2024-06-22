b9e8c6122292ae93b1e8472b912447bc5cec7785
denosawr <olliecheng@outlook.com>
2024-06-22 22:48:27 +1000

    For now, set /upload to cache subject JSONs in the server/cache directory

--- server/lookahead_server/api/api_routes.py
+++ server/lookahead_server/api/api_routes.py
@@ -4,6 +4,7 @@ import asyncio
 
 import logging
 import os
+import json
 
 from lookahead_server import parsing
 
@@ -20,12 +21,25 @@ routes = web.RouteTableDef()
 
 @routes.post("/upload")
 async def upload(req):
-    asyncio.shield(upload_and_mutate(req))
-    return web.Response(text="Successfully uploaded")
+    if "code" not in req.query:
+        return web.HTTPBadRequest(
+            text="Missing `code` query parameter (i.e. MAST10009_SM2_2024)"
+        )
+    if not req.can_read_body:
+        return web.HTTPBadRequest(text="Missing body.")
+
+    return await asyncio.shield(upload_and_mutate(req))
 
 
 async def upload_and_mutate(req):
-    pass
+    data = await req.json()
+    try:
+        with open(f"cache/{req.query['code']}.json", "w") as f:
+            json.dump(data, f)
+
+        return web.Response(text="Successfully uploaded")
+    except:
+        return web.HTTPBadRequest(text="Malformed body data.")
 
 
 @routes.get("/getSubject")
